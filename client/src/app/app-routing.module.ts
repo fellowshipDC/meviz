68da7c321d66a39bb274a52f18d748b9790ed6a7
Rolando Escalona <rolando.escalona@gmail.com>
2017-12-19 11:58:17 -0600

    hotfix

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { RadialchartComponent } from './radialchart/radialchart.component';
-import { RadarchartComponent } from './radarchart/radarchart.component';
+
 import { BarchartComponent } from './barchart/barchart.component';
 
 
@@ -10,10 +10,6 @@ const routes: Routes = [
   {
     path: 'radialchart',
     component: RadialchartComponent
-  },
-  {
-    path: 'radarchart',
-    component: RadarchartComponent
   },
   {
     path: 'barchart',
